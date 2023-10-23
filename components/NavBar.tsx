d46afe79101a9305beba9335c918ca4055e828a6
PHẠM NGỌC HUY <phamngochuy@MacBook-Air-cua-PHAM.local>
2023-10-24 00:19:09 +0700

    first upload

---
+++ components/NavBar.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import React from "react";
+import { UserButton } from "@clerk/nextjs";
+
+function NavBar() {
+  return (
+    <div className="hidden md:flex item-center justify-around py-3 shadow-md border-b-[1px]">
+      <Image src="./next.svg" alt="logo" width={100} height={150} />
+
+      <div className="flex gap-5">
+        <h2 className="hover:bg-blue-500 px-3 cursor-pointer p-2 rounded-full hover:text-white">
+          Home
+        </h2>
+        <h2 className="hover:bg-blue-500 px-3 cursor-pointer p-2 rounded-full hover:text-white">
+          History
+        </h2>
+        <h2 className="hover:bg-blue-500 px-3 cursor-pointer p-2 rounded-full hover:text-white">
+          Contact us
+        </h2>
+      </div>∑
+
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
+}
+
+export default NavBar;
